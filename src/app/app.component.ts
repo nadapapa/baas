ea48752b03f6e20bbfc1d8eb9e8eee5af7a30c13
Bence Turi <tbence3@gmail.com>
2017-06-23 21:50:49 +0200

    user profile at nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 import {EventService} from './event.service';
 import { Router } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,17 @@ export class AppComponent implements OnInit {
   title;
 
   user: Observable<firebase.User>;
+  public isLoggedIn: boolean;
+  public userProfile = {name: null, img: null};
 
   constructor(public afAuth: AngularFireAuth, private eventService: EventService, private router: Router) {
     this.user = afAuth.authState;
     this.eventService.changeEmitted$.subscribe(message => {
       this.title = typeof message.title === 'undefined' ? message.title : '';
     });
+
+    this.isLoggedIn = false;
+    this.setUserParams();
   }
 
   ngOnInit() {
@@ -28,8 +34,17 @@ export class AppComponent implements OnInit {
 
   public logout() {
     this.afAuth.auth.signOut();
-    this.router.navigate(['/']);
+    this.setUserParams();
+    this.router.navigateByUrl('/');
   }
 
+  setUserParams() {
+      this.afAuth.authState.subscribe(user => {
+        this.userProfile.name = user.displayName;
+        this.userProfile.img = user.photoURL;
+      });
+
+      this.isLoggedIn = true;
+  }
 
 }
