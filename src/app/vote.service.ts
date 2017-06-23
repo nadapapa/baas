b26dda644717c1daadc46ef340b8e33a216a5e45
Kovács Balázs <balazs.kovacs@bigfish.hu>
2017-06-23 21:34:18 +0200

    /me

--- src/app/vote.service.ts
+++ src/app/vote.service.ts
@@ -21,6 +21,12 @@ export class VoteService {
     return this.votes;
   }
 
+  public getByEmail(email) {
+    return this.votes.map(voteList => {
+      return voteList.filter(item => item.from === email || item.to === email);
+    });
+  }
+
   public getGroupedVotes(boardId) {
     return this.votes.map(voteList => {
       const filtered = voteList.filter(item => item.board === boardId);
@@ -39,8 +45,6 @@ export class VoteService {
           grouped[item.to][item.category] = 1;
         }
         let sum = 0;
-        console.log(grouped[item.to]);
-
 
         for (const key in grouped[item.to]) {
           if (grouped[item.to].hasOwnProperty(key)) {
@@ -59,31 +63,4 @@ export class VoteService {
     });
   }
 
-  /*
-  public getByEmailBoard(boardId, votes) {
-      const filtered = votes.filter(item => item.board === boardId);
-      const grouped = {};
-      filtered.forEach(item => {
-        if (grouped.hasOwnProperty(item.to)) {
-          if (grouped[item.to].hasOwnProperty(item.category)) {
-            grouped[item.to][item.category] += 1;
-          } else {
-            grouped[item.to][item.category] = 1;
-          }
-        } else {
-          grouped[item.to] = {};
-          grouped[item.to][item.category] = 1;
-        }
-      });
-      console.log(grouped);
-/*
-    return {
-      'votePercentage': 4.5,
-      'votes': {
-        0: 10,
-        1: 21
-      }
-    };
-
-  }*/
 }
