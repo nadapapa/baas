77542a1eb89bfd4c80ea801baafedcded367370d
Bence Turi <tbence3@gmail.com>
2017-06-23 14:32:14 +0200

    Google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/map';
+import { Component, OnInit, } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  user: Observable<firebase.User>;
 
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  public logout() {
+    this.afAuth.auth.signOut();
   }
 
   ngOnInit() {
