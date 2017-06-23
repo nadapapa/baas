ae10f8ea1cae732236694185959936bbb33aa926
Bence Turi <tbence3@gmail.com>
2017-06-24 01:20:37 +0200

    archive

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,7 +6,10 @@ import { BoardService } from '../board.service';
 
 import 'rxjs/add/operator/switchMap';
 import { VoteComponent } from '../vote/vote.component';
-import {EventService} from '../event.service';
+import { EventService } from '../event.service';
+import { VoteService } from '../vote.service';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-board',
@@ -15,19 +18,38 @@ import {EventService} from '../event.service';
 })
 export class BoardComponent implements OnInit {
   private board;
+  public userEmail;
+  public adminUser;
 
   constructor(private BoardService: BoardService, private route: ActivatedRoute, public dialog: MdDialog,
-              private eventService: EventService) { }
+              private eventService: EventService, private voteService: VoteService, public afAuth: AngularFireAuth) {
+
+    afAuth.authState.map(user => {
+      this.userEmail = user.email;
+    }).subscribe();
+  }
 
   ngOnInit(): void {
     this.eventService.emitChange({title: 'Felhasználók'});
-
     this.route.params
         .switchMap((params: Params) => this.BoardService.getBoard(params['id']))
-        .subscribe(data => this.board = data);
+        .subscribe(data => {
+          this.board = data;
+          this.adminUser = this.board.users.filter( item => (item.email == this.userEmail && item.is_admin == true))[0];
+        });
   }
 
   private openVote(email: string) {
     const dialogRef = this.dialog.open(VoteComponent, {data: {board: this.board, email: email}, width: '80%'});
   }
+
+  archiveBt(id: number) {
+    this.voteService.archiveVotesByBoard(id);
+  }
+
+  isAdcmin() {
+    const filter = this.board.users.filter( item => (item.email == this.userEmail && item.is_admin == true) );
+    return filter.length === 1;
+  }
+
 }
